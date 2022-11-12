8b0e486e3adebfa82960794c20f1bb12372c4ff7
ostnam <tsougaevm@gmail.com>
2022-11-12 14:17:50 +0100

    implemented login

--- server.py
+++ server.py
@@ -1,16 +1,40 @@
 import flask
+import flask_login
 
-import feed
+import login
 import datamanager
 
 app = flask.Flask(__name__)
 data_manager = datamanager.DataManager()
 
+app.secret_key = b"6de9bbfe2e1acf239e2f"
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login_page"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return login.User.get(user_id)
+
 @app.route("/")
 def index():
-    return flask.redirect(flask.url_for("/feeds"))
+    return flask.redirect(flask.url_for("feeds"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    if flask.request.method == "GET":
+        return flask.render_template(
+            "login_page.html"
+        )
+    elif flask.request.method ==  "POST":
+        submitted_token = flask.request.values.get("token")
+        user = login.User.get(submitted_token)
+        flask_login.login_user(user, remember=True)
+        return flask.redirect(flask.url_for("feeds"))
 
 @app.route("/feeds")
+@flask_login.login_required
 def feeds():
     feeds = data_manager.get_subscribed_feeds()
     entries = data_manager.get_entries(feeds)
@@ -21,3 +45,8 @@ def feeds():
         feeds_dict=feeds_dict
     )
 
+@app.route("/logout")
+@flask_login.login_required
+def logout():
+    flask_login.logout_user()
+    return "logged out"
