493fa4056e5dabdee15b80ed0948b52e2a7aedb6
ostnam <tsougaevm@gmail.com>
2022-12-27 17:03:11 +0100

    main server methods

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main (main) where
 
 import Web.Scotty
 import Network.Wai.Middleware.HttpAuth
-import System.Environment (getEnv)
+import System.Environment (getEnv, lookupEnv)
 import Network.Wai ( Request, pathInfo, Middleware )
 import Data.ByteString.Internal (c2w)
 import Network.Wai.Middleware.Static
+import Control.Monad (mapM, liftM)
+import Control.Monad.IO.Class (liftIO)
+import Control.Concurrent ( forkIO )
+import qualified Data.Aeson as Aeson
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.UTF8 as BL
+import qualified Database.SQLite.Simple as DB
+import qualified Data
+import qualified Types
+
+import Debug.Trace (trace)
 
 authSettings :: AuthSettings
 authSettings = "MPFS" { authIsProtected = needsAuth }
@@ -27,13 +37,60 @@ checkCreds shouldBeUser shouldBePw username password = return $
 auth :: String -> String -> Middleware
 auth userId sessionId = basicAuth (checkCreds userId sessionId) authSettings
 
+getDbPath :: () -> IO String
+getDbPath _ = do
+  isEnv <- lookupEnv "MPFS_PRODUCTION"
+  case isEnv of
+    Just _ -> return "/data/feed.db"
+    _      -> return "./feed.db"
+
 main :: IO ()
 main = do
   userId <- getEnv "MPFS_USERID"
   sessionId <- getEnv "MPFS_SESSION_KEY"
+  dbPath <- getDbPath ()
+
+  conn <- DB.open dbPath
+  Data.setUpDb conn
+
+  forkIO $ Data.refreshLoop conn
+
   scotty 8080 $ do
     middleware $ auth userId sessionId
     middleware $ staticPolicy $ hasPrefix "static"
 
     get "/" $ redirect "/feeds"
     get "/feeds" $ setHeader "Content-Type" "text/html" >> file "templates/feeds.html"
+    post "/seen" $ do
+      reqBody <- body
+      case Aeson.decode reqBody of
+        Just item -> liftIO $ Data.markSeen conn item
+        _         -> return ()
+      text ""
+
+    post "/subscriptions" $ do
+      reqBody <- body
+      case Aeson.decode reqBody of
+        Just feed -> liftIO $ Data.addFeed conn feed
+        _ ->  return $ trace (show reqBody) ()
+      text ""
+
+    delete "/subscriptions" $ do
+      reqBody <- body
+      case Aeson.decode reqBody of
+        Just feed -> liftIO $ Data.deleteFeed conn feed
+        _ ->  return ()
+      text ""
+
+    get "/subscriptions" $ do
+      feeds <- liftIO $ Data.getFeeds conn
+      json feeds
+
+    post "/entries_batch" $ do
+      reqBody <- body
+      items <- case Aeson.decode reqBody of
+        Just (feeds :: [Types.Feed]) -> concat <$> mapM (liftIO . Data.getItems conn) feeds
+        _          -> return []
+      json items
+
+  DB.close conn
